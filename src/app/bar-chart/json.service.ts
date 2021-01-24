c0c289f975524cfaf4974cb3e117f634856248eb
Oskrabble <oskarquirozvento@gmail.com>
2021-01-24 14:15:48 -0500

    Update json.service.ts

--- src/app/bar-chart/json.service.ts
+++ src/app/bar-chart/json.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import {LoginService} from './../login/service/login.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonService {
 
-  constructor(private http: HttpClient) { }
+  httpOptions : any
+  token
+
+  constructor(private http: HttpClient,private service :LoginService) { 
+    this.token = this.service.getToken()
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'access-token': this.token
+      })
+    };
+  }
 
   getJson(url: string){
-    return this.http.get(url);
+    return this.http.get(url ,this.httpOptions);
   }
 }
